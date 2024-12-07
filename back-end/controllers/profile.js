067d5ad436c6b206edcb6eaea11d1ded3e1389a9
gittbin <tranledung2907@gmail.com>
2024-12-07 10:55:14 +0700

    update

--- back-end/controllers/profile.js
+++ back-end/controllers/profile.js
@@ -23,7 +23,35 @@ const get_profile=async (req, res) => {
         res.status(500).json({ message: 'Error logging in', error });
     }
 }
-
+const change_profile=async(req,res)=>{
+    const { user } = req.body;
+    try{const updated_user=await User.findByIdAndUpdate(
+        {_id:user._id},
+    {$set:{name:user.name,password:user.password}},
+    { new: true })
+res.status(200).json({respond:"success"})
+}catch (err) {
+        console.error(err)
+        res.status(500).json({ respond:"error" });
+    }
+    
+}
+const update_avatar= async (req,res) => {
+    const {user,newPr}=req.body;
+    console.log(user,newPr)
+    try{const updated_user=await User.findByIdAndUpdate(
+        {_id:user._id},
+    {$set:{avatar:newPr.image.secure_url}},
+    { new: true })
+    console.log(updated_user)
+res.status(200).json({respond:"success"})
+}catch (err) {
+        console.error(err)
+        res.status(500).json({ respond:"error" });
+    }
+}
 module.exports = {
     get_profile,
+    change_profile,
+    update_avatar
 }
--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -4,5 +4,6 @@ const profile = require('../controllers/profile.js'); // Import controller
 const router = express.Router();
 
 router.post('/get_profile', profile.get_profile);
-
+router.post('/change_profile', profile.change_profile);
+router.post('/update_avatar', profile.update_avatar);
 module.exports = router;
