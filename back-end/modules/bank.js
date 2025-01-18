29e830823003084869faf7eee463007571dcfe1d
gittbin <tranledung2907@gmail.com>
2025-01-19 00:56:01 +0700

    update

--- back-end/modules/bank.js
+++ back-end/modules/bank.js
@@ -5,9 +5,6 @@ const BankAccountSchema = new mongoose.Schema({
   name:{type:String,required:true},
   bankName: { type: String, required: true },
   accountNumber: { type: String, required: true },
-  image: { secure_url: String,
-    public_id: String,
-             }, // Lưu đường dẫn ảnh QR
 }, { timestamps: true });
 const Banks = mongoose.model('BankAccounts', BankAccountSchema,'BankAccounts');
 module.exports = Banks;
